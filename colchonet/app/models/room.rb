6159833f210859070d2d52ad9047276becaa10f8
Vander Lima de Andrade <vnd.vander@gmail.com>
2016-07-20 01:49:05 -0300

    commit 8/12

--- colchonet/app/models/room.rb
+++ colchonet/app/models/room.rb
@@ -1,4 +1,10 @@
 class Room < ActiveRecord::Base
+
+		validates_presence_of :title, :location, :description
+		validates_length_of :description, minium: 10, allow_blank: false
+
+
+
 	def complete_name
 		"#{title}, #{location}"
 		
--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -1,11 +1,29 @@
 class User < ActiveRecord::Base
 	#EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[@\.]+\z/
 
-	validates_presence_of :email, :full_name, :location, :password
+	validates_presence_of :email, :full_name, :location
 	#validates_confirmation_of :password
 	validates_length_of :bio, minium: 30, allow_blank: false
 	validates_uniqueness_of :email
 	has_secure_password
+
+	before_create do |user|
+		user.confirmation_token = SecureRandom.urlsafe_base64
+	end
+
+	def confirm!
+		return if confirmed?
+
+		self.confirmed_at = Time.current
+		self.confirmation_token = ''
+		save!
+		
+	end
+
+	def confirmed?
+		confirmed_at.present?
+		
+	end
 	#Essa validação pode ser representada da seguinte forma:
 	 #validates_format_of :email, with EMAIL_REGEXP
 	#validate do 
